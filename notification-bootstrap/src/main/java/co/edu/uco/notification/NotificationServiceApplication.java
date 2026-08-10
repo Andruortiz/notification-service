f2c37bd9bd1e3999dcccd48e04fecbdc459aa694
Eliudcampos <elicampos321@gmail.com>
2026-08-10 00:37:40 -0500

    refactor: move application class to base package
    Estaba en co.edu.uco.notification.bootstrap, asi que el escaneo de componentes
    arrancaba ahi y no habria encontrado los beans de adapters. Spring Modulith
    toma ademas el paquete de esta clase como raiz para descubrir los modulos.

--- notification-bootstrap/src/main/java/co/edu/uco/notification/NotificationServiceApplication.java
+++ notification-bootstrap/src/main/java/co/edu/uco/notification/NotificationServiceApplication.java
@@ -1,12 +1,23 @@
-package co.edu.uco.notification.bootstrap;
+package co.edu.uco.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.modulith.Modulithic;
 
+/**
+ * Punto de arranque del componente de notificaciones.
+ *
+ * <p>La clase vive en el paquete raíz {@code co.edu.uco.notification} de forma deliberada: el
+ * escaneo de componentes parte de aquí, de modo que alcanza los adaptadores, y Spring Modulith
+ * toma este paquete como raíz para descubrir los módulos de la aplicación.
+ */
+@Modulithic(systemName = "Notification Service", sharedModules = "shared")
 @SpringBootApplication
+@ConfigurationPropertiesScan("co.edu.uco.notification")
 public class NotificationServiceApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
 }
